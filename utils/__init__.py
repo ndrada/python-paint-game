376be5fbde930e61dee5630cb0606e7f00d919ee
ndrada <andrada.groseanu@gmail.com>
2023-10-28 13:58:52 -0700

    full program

---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+from .settings import * #import everything from the settings file
+from .buttons import Button
+import pygame
+pygame.init() #initialize pygame
+pygame.font.init() #initialize font library within pygame
+
+
